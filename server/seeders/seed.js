a7339b42516a8ed1f5ecc83d2fbecc2c505abc0b
stephje <s.jenkins3018@gmail.com>
2021-08-13 11:09:43 +0800

    Updated seed script to account for roles that have no specific qualifications

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -18,17 +18,21 @@ db.once('open', async () => {
       await Role.deleteMany({});
 
       for (const role of roleSeeds) {
-        qualificationsArray = role.qualifications;
+        if(role.qualifications) {
+          qualificationsArray = role.qualifications;
+          let newQualificationsArray = [];
 
-        let newQualificationsArray = [];
-
-        for (const qualification of qualificationsArray) {
-          const qualificationObject = await qualificationsCollection.findOne({name: qualification});
-          const qualificationId = qualificationObject._id
-          newQualificationsArray.push(qualificationId);
+          for (const qualification of qualificationsArray) {
+            const qualificationObject = await qualificationsCollection.findOne({name: qualification});
+            const qualificationId = qualificationObject._id
+            newQualificationsArray.push(qualificationId);
+          }
+          role.qualifications = newQualificationsArray;
+          await Role.create(role);
+        } else if (!role.qualifications) {
+          await Role.create(role);
         }
-        role.qualifications = newQualificationsArray;
-        await Role.create(role);
+
       }
   
       console.log('Database Seeded');
