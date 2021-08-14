afda2ff63837e71072407f9c3a1f04b8ad7d44a1
stephje <s.jenkins3018@gmail.com>
2021-08-14 18:11:30 +0800

    Fixed styling of login and signup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,134 +7,144 @@ import Auth from '../utils/auth';
 import AppBar from '../components/AppBar';
 import StickyFooter from '../components/StickyFooter';
 
-import { 
-  Avatar,
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  CssBaseline,
-  FormControlLabel,
-  Grid,
-  Link,
-  makeStyles,
-  TextField,
-  Typography
-} from '@material-ui/core'
+import {
+    Avatar,
+    Box,
+    Button,
+    Checkbox,
+    Container,
+    CssBaseline,
+    FormControlLabel,
+    Grid,
+    Link,
+    makeStyles,
+    TextField,
+    Typography,
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
-
 //Styling from Material-UI template
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 export default function Login() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [formState, setFormState] = useState({ email: '', password: ''});
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+    // update state based on form input changes
+    const handleChange = event => {
+        const { name, value } = event.target;
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const { data } = await login({
-        variables: { ...formState },
-      });
+    // submit form
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
 
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
-    }
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.error(e);
+        }
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
-  };
-  
-  return (
-    <Container component="main" maxWidth="xs">
-    <AppBar />
-    <CssBaseline />
-    <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Sign in
-      </Typography>
-      <form onSubmit={handleFormSubmit} className={classes.form} noValidate>
-        <TextField
-          onChange={handleChange}
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          onChange={handleChange}
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-        >
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item>
-            <Link component={RouterLink} to="/signup" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
-    </div>
-  </Container>
-  );
+        // clear form values
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
+
+    return (
+        <Container disableGutters maxWidth={false}>
+            <AppBar />
+            <Container component='main' maxWidth='xs'>
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component='h1' variant='h5'>
+                        Sign in
+                    </Typography>
+                    <form
+                        onSubmit={handleFormSubmit}
+                        className={classes.form}
+                        noValidate
+                    >
+                        <TextField
+                            onChange={handleChange}
+                            variant='outlined'
+                            margin='normal'
+                            required
+                            fullWidth
+                            id='email'
+                            label='Email Address'
+                            name='email'
+                            autoComplete='email'
+                            autoFocus
+                        />
+                        <TextField
+                            onChange={handleChange}
+                            variant='outlined'
+                            margin='normal'
+                            required
+                            fullWidth
+                            name='password'
+                            label='Password'
+                            type='password'
+                            id='password'
+                            autoComplete='current-password'
+                        />
+                        <Button
+                            type='submit'
+                            fullWidth
+                            variant='contained'
+                            color='primary'
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item>
+                                <Link
+                                    component={RouterLink}
+                                    to='/signup'
+                                    variant='body2'
+                                >
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+            <StickyFooter />
+        </Container>
+    );
 }
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,4 @@
-import { 
+import {
     Avatar,
     Box,
     Button,
@@ -10,9 +10,11 @@ import {
     Grid,
     makeStyles,
     TextField,
-    Typography
-} from '@material-ui/core'
+    Typography,
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import AppBar from '../components/AppBar';
+import StickyFooter from '../components/StickyFooter';
 
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
@@ -23,28 +25,27 @@ import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 //Styling from Material-UI template
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
     paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
     avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
     },
     submit: {
-      margin: theme.spacing(3, 0, 2),
+        margin: theme.spacing(3, 0, 2),
     },
-  }));  
+}));
 
 export default function Signup() {
-    
     const classes = useStyles();
 
     const [formState, setFormState] = useState({
@@ -52,101 +53,113 @@ export default function Signup() {
         email: '',
         password: '',
         admin: false,
-      });
-      const [addUser, { error, data }] = useMutation(ADD_USER);
-    
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-    
-        setFormState({
-          ...formState,
-          [name]: value,
-        });
-      };
-    
-      const handleFormSubmit = async (event) => {
-        event.preventDefault();
-    
-        try {
-          const { data } = await addUser({
-            variables: { ...formState },
-          });
-    
-          Auth.login(data.addUser.token);
-        } catch (e) {
-          console.error(e);
-        }
-      };
+    });
+    const [addUser, { error, data }] = useMutation(ADD_USER);
 
-      //Components and design based on Material-UI template
-      return(
-        <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <form onSubmit={handleFormSubmit} className={classes.form} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  onChange={handleChange}
-                  name="username"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  onChange={handleChange}
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  onChange={handleChange}
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link component={RouterLink} to="/login" variant="body2">
-                  Already have an account? Log in
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-      )
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    //Components and design based on Material-UI template
+    return (
+        <Container disableGutters maxWidth={false}>
+            <AppBar />
+            <Container component='main' maxWidth='xs'>
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component='h1' variant='h5'>
+                        Sign up
+                    </Typography>
+                    <form
+                        onSubmit={handleFormSubmit}
+                        className={classes.form}
+                        noValidate
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    onChange={handleChange}
+                                    name='username'
+                                    variant='outlined'
+                                    required
+                                    fullWidth
+                                    id='username'
+                                    label='Username'
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    onChange={handleChange}
+                                    variant='outlined'
+                                    required
+                                    fullWidth
+                                    id='email'
+                                    label='Email Address'
+                                    name='email'
+                                    autoComplete='email'
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    onChange={handleChange}
+                                    variant='outlined'
+                                    required
+                                    fullWidth
+                                    name='password'
+                                    label='Password'
+                                    type='password'
+                                    id='password'
+                                    autoComplete='current-password'
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type='submit'
+                            fullWidth
+                            variant='contained'
+                            color='primary'
+                            className={classes.submit}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent='flex-end'>
+                            <Grid item>
+                                <Link
+                                    component={RouterLink}
+                                    to='/login'
+                                    variant='body2'
+                                >
+                                    Already have an account? Log in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+            <StickyFooter />
+        </Container>
+    );
 }
