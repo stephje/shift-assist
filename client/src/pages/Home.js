ab0886ca7d28e5791141e746ae7d672b73626e25
stephje <s.jenkins3018@gmail.com>
2021-08-14 21:27:41 +0800

    Minor styling changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Box, Container, Typography } from '@material-ui/core';
+import { Box, Container, Typography, Grid } from '@material-ui/core';
 import StickyFooter from '../components/StickyFooter';
 import AppBar from '../components/AppBar';
+import logo from '../images/shiftassist.png';
 
 export default function Home() {
   return (
     <Container disableGutters maxWidth={false}>
       <AppBar />
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Hello, World
-        </Typography>
-      </Box>
+        <Container>
+        <Grid container justifyContent='center'>
+        <Box>
+          <img src={logo} />
+          </Box>
+        </Grid>
+        </Container>
       <StickyFooter />
     </Container>
   );
