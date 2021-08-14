7c8ada24cbd2e04285a6c1a6ea9721012df3993f
stephje <s.jenkins3018@gmail.com>
2021-08-14 21:29:08 +0800

    removed unnecessary line

--- server/server.js
+++ server/server.js
@@ -24,8 +24,6 @@ async function startApolloServer() {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    app.use('/images', express.static(path.join(__dirname, '../client/images')));
-
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/build')));
       }
